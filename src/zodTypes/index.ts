6037b145cc67261e0a9756e5dc0f6b8531a4e9e2
rubcube-guilhermetomiasi <guilherme.tomiasi@rubcube.com>
2023-05-10 10:18:23 -0300

    feat: validadores de CPF e CNPJ
    Criando tipo zod genérico para strings numéricas

--- src/zodTypes/index.ts
+++ src/zodTypes/index.ts
@@ -2,3 +2,4 @@ import { z } from 'zod';
 
 export const bcryptHash = z.string().length(60);
 export const parsedDate = z.string().datetime().or(z.date()).transform(date => new Date(date));
+export const numericString = (errorMsg: string) => z.string().regex(/\d*/, errorMsg);
--- src/zodTypes/user.ts
+++ src/zodTypes/user.ts
@@ -1,4 +1,6 @@
 import { z } from 'zod';
+import { validateCNPJ, validateCPF } from './customValidators';
+import { numericString } from '.';
 
 export const userName = z
     .string()
@@ -7,15 +9,16 @@ export const userEmail = z
     .string()
     .email("Endereço de e-mail não obedece o formato especificado.")
     .max(320, "Endereço de e-mail excede 320 caracteres.");
-export const userPhone = z
-    .string()
-    .regex(/\d+/, "Número de telefone contém caracteres não-numéricos")
+export const userPhone = numericString("Número de telefone contém caracteres não-numéricos")
     .max(13, "Número de telefone excede 13 dígitos");
-export const userDocument = z
-    .string()
-    .min(11, "Documento não contém mínimo de 11 dígitos")
-    .max(14, "Documento excede 14 dígitos");
+const userCPF = numericString("CPF contém caracteres não-numéricos")
+    .length(11)
+    .refine(validateCPF, "CPF inserido não é válido");
+const userCNPJ = numericString("CNPJ contém caracteres não-numéricos")
+    .length(14)
+    .refine(validateCNPJ, "CNPJ inserido não é válido");
+export const userDocument = userCPF.or(userCNPJ);
 export const userPassword = z
     .string()
-    .min(8)
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+    .min(8, "Senha deve ter ao menos 8 caracteres")
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "Senha deve conter letras, números e caracteres especiais");
