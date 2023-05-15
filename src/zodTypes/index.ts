76ea403e2cd3054b1b90e3ac76daaabee08552eb
rubcube-guilhermetomiasi <guilherme.tomiasi@rubcube.com>
2023-05-15 10:24:33 -0300

    fix: permitindo data simples no zodType parsedDate
    Antes só formato datetime era aceito

--- src/zodTypes/index.ts
+++ src/zodTypes/index.ts
@@ -3,8 +3,12 @@ import { z } from "zod";
 export const bcryptHash = z.string().length(60);
 export const parsedDate = z
   .string()
-  .datetime()
+  .regex(
+    /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/,
+    "Formato de data é inválido",
+  )
   .or(z.date())
+  .or(z.string().datetime())
   .transform(date => new Date(date));
 export const numericString = (errorMsg: string) =>
-  z.string().regex(/\d*/, errorMsg);
+  z.string().regex(/^\d*$/, errorMsg);
