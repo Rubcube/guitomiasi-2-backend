bbaa4c2eb61c139121160d0608874e2ab8e6e9cf
rubcube-guilhermetomiasi <guilherme.tomiasi@rubcube.com>
2023-05-19 12:41:31 -0300

    aprimorando DTO de requisição de extrato
    Dessa maneira, o ZodJS fica responsável por verificar.
    Verifica se o intervalo de data é válido.
    Verifica se o número da página é válido.

--- src/dtos/DateDTO.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { parsedDate } from "zodTypes/index";
-
-export const DateRangeSchema = z.object({
-  start: parsedDate.optional(),
-  end: parsedDate.optional(),
-});
-
-export type DateRange = z.infer<typeof DateRangeSchema>;
--- src/dtos/TransferDTO.ts
+++ src/dtos/TransferDTO.ts
@@ -1,14 +1,43 @@
+import { DateTime } from "luxon";
 import { z } from "zod";
 import {
   accountNumber,
   monetaryValue,
   transactionPassword,
 } from "zodTypes/account";
+import { parsedDate } from "zodTypes/index";
 
 export const TransferInSchema = z.object({
   account_number_to: accountNumber,
   value: monetaryValue,
   transactional_password: transactionPassword,
+  time_to_transfer: parsedDate.optional(),
 });
 
+export const TransferOutSchema = z
+  .object({
+    page: z.coerce.number().gte(0).optional(),
+    start: parsedDate.optional(),
+    end: parsedDate.optional(),
+  })
+  .superRefine((obj, ctx) => {
+    if (obj.start && obj.end) {
+      const startDate = DateTime.fromJSDate(obj.start);
+      const endDate = DateTime.fromJSDate(obj.end);
+      const delta = endDate.diff(startDate, "day").days;
+
+      if (delta >= 0) {
+        return obj;
+      } else {
+        ctx.addIssue({
+          code: "custom",
+          path: ["start", "end"],
+        });
+
+        return z.NEVER;
+      }
+    }
+  });
+
 export type TransferIn = z.infer<typeof TransferInSchema>;
+export type TransferOut = z.infer<typeof TransferOutSchema>;
