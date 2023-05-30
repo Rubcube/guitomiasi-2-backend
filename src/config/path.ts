a598ecc22604b47d8e6bf0bf4ceb964082ec6daf
rubcube-guilhermetomiasi <guilherme.tomiasi@rubcube.com>
2023-05-30 14:19:07 -0300

    enhance: modularizando path da URL, template email

---
+++ src/config/path.ts
@@ -0,0 +1,7 @@
+/**
+ * Configurações relacionadas ao path da aplicação.
+ * Utilizado para atualizar o URL com base na URL base
+ */
+export const PathConfig = {
+  BASE_PATH: process.env.BASE_URL as string,
+};
--- src/services/mail.ts
+++ src/services/mail.ts
@@ -1,6 +1,22 @@
 import { MailTrapConfig } from "config/mailtrap";
+import { PathConfig } from "config/path";
+import { readFileSync } from "fs";
+import Handlebars from "handlebars";
 import { createTransport } from "nodemailer";
+import { resolve } from "path";
 import { signJWT } from "./jwt";
+
+const templateFile = readFileSync(
+  resolve(__dirname, "mailTemplate.handlebars"),
+  "utf-8",
+);
+const emailTemplate = Handlebars.compile(templateFile);
+interface EmailTemplateContext {
+  title: string;
+  main_message: string;
+  button_href: string;
+  button_text: string;
+}
 
 /**
  * Transport mailer criado pelo `nodemailer` para envio de emails.
@@ -21,15 +37,35 @@ export const mailerTransport = createTransport({
  * um determinado email.
  * @param to Email de destino
  * @param subject Assunto do email
- * @param message Texto do email
+ * @param text Texto do email
  */
-export function sendMail(to: string, subject: string, message: string) {
+export function sendMail(
+  to: string,
+  subject: string,
+  content: { text?: string; html?: string },
+) {
   mailerTransport.sendMail({
     from: "noreply@rubbank.com",
     to,
     subject,
-    text: message,
+    text: content.text,
+    html: content.html,
   });
+}
+
+/**
+ * Envia um email a partir do endereço "noreply@rubbank.com" para
+ * um determinado email, utilizando o template handlebars.
+ * @param emailTo Email de destino
+ * @param subject Assunto do email
+ * @param context Contexto do template
+ */
+export function sendMailTemplate(
+  emailTo: string,
+  subject: string,
+  context: EmailTemplateContext,
+) {
+  sendMail(emailTo, subject, { html: emailTemplate(context) });
 }
 
 /**
@@ -41,19 +77,35 @@ export function sendMail(to: string, subject: string, message: string) {
 export function sendVerificationMail(emailTo: string) {
   const emailedJWT = signJWT({ email: emailTo }, 86400);
 
-  sendMail(
-    emailTo,
-    "Please verify your email",
-    `0.0.0.0:3344/user/verify/${emailedJWT}`,
-  );
+  sendMailTemplate(emailTo, "Verifique seu email", {
+    title: "Bem-vindo ao RubBank!",
+    main_message:
+      "Clique no botão abaixo para verificar seu email e começar a usar sua conta",
+    button_href: `${PathConfig.BASE_PATH}/user/verify/${emailedJWT}`,
+    button_text: "Verificar email",
+  });
 }
 
-export function sendResetPasswordMail(id: string, emailTo: string) {
-  const emailedJWT = signJWT({ id }, 3600);
-
-  sendMail(
-    emailTo,
-    "Password change requested",
-    `0.0.0.0:3344/user/password/new/${emailedJWT}`,
+/**
+ * Envia um email de **reset** de senha para um determinado email.
+ * @param id ID do usuário a ter sua senha resetada
+ * @param emailTo Email do usuário a ter sua senha resetada
+ */
+export function sendResetPasswordMail(
+  id: string,
+  emailTo: string,
+  currentPasswordHash: string,
+) {
+  const emailedJWT = signJWT(
+    { id, email: emailTo, old_password: currentPasswordHash },
+    3600,
   );
+
+  sendMailTemplate(emailTo, "Password change requested", {
+    title: "Solicitação de troca de senha",
+    main_message:
+      "Uma solicitação de troca de senha foi enviada ao nosso serviço. Se você não realizou essa solicitação, ignore essa mensagem de email. Para prosseguir, clique no botão abaixo.",
+    button_href: `${PathConfig.BASE_PATH}/user/password/new/${emailedJWT}`,
+    button_text: "Trocar senha",
+  });
 }
